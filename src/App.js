d684dfdf13eecbd70d677613f97bdf1935e265a9
n4mhoon <66673866+n4mhoon@users.noreply.github.com>
2020-08-12 19:38:41 +0900

    add new file

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
   );
-}
+};
+
+const Header = () => {
+  return <div>Header</div>;
+};
+
+const Content = () => {
+  return <div>Content</div>;
+};
+
+const Footer = () => {
+  return <div>Footer</div>;
+};
 
 export default App;
+
+// 컴포넌트를 만들때는 arrow function, Custom Hook을 만들때는 function
+// const App = () => {}
+// function
+
+// <Content /> === <Content> </Content>
+// <header className="App-header">
+// <img src={logo} className="App-logo" alt="logo" />
+// <p>
+//   Edit <code>src/App.js</code> and save to reload.
+// </p>
+// <a
+//   className="App-link"
+//   href="https://reactjs.org"
+//   target="_blank"
+//   rel="noopener noreferrer"
+// >
+//   Learn React
+// </a>
+// </header>
