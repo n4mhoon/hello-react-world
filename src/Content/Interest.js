f3c3731820eef853ea97854a9cc5b3b9be838dd2
n4mhoon <66673866+n4mhoon@users.noreply.github.com>
2020-10-12 18:47:48 +0900

    fix: responsive iframe

--- src/Content/Interest.js
+++ src/Content/Interest.js
@@ -7,50 +7,59 @@ export const Interest = () => {
     <>
       <ContentImage />
       <ContentWrapper>
-      <ContentInfo>
-        <span role="img" aria-label="Pallete">
-          🎨
-        </span>
-        <h1>interest</h1>
-        <p className="explain">
-          "다양한 활동에 도전하는 편입니다." <br />
-        </p>
-      </ContentInfo>
-      <ContentArticle>
-        <h2>Making contents</h2>
-        <iframe
-          title="ipad opening"
-          src="https://player.vimeo.com/video/374371778"
-          width="640px"
-          height="360px"
-          frameBorder="0"
-          allow="autoplay; fullscreen"
-        ></iframe>
-        <br /> <br /> <br />
-        <h2>Playing Game</h2>
-        <iframe
-          title="beatsaber"
-          src="https://player.vimeo.com/video/375063587"
-          width="640px"
-          height="360px"
-          frameBorder="0"
-          allow="autoplay; fullscreen"
-        ></iframe>
-        <br /> <br /> <br />
-        <h2>Do something.. fun!</h2>
-        <iframe
-          title="play kalimba"
-          src="https://player.vimeo.com/video/375051982"
-          width="640px"
-          height="360px"
-          frameBorder="0"
-          allow="autoplay; fullscreen"
-        ></iframe>
-        <br />
-        <br />
-        <br />
-        <br />
-      </ContentArticle>
+        <ContentInfo>
+          <span role="img" aria-label="Pallete">
+            🎨
+          </span>
+          <h1>interest</h1>
+          <p className="explain">
+            "다양한 활동에 도전하는 편입니다." <br />
+          </p>
+        </ContentInfo>
+        <ContentArticle>
+          <h2>Making contents</h2>
+          <div class="container">
+            <iframe
+              class="responsive-iframe"
+              title="ipad opening"
+              src="https://player.vimeo.com/video/374371778"
+              width="640px"
+              height="360px"
+              frameBorder="0"
+              allow="autoplay; fullscreen"
+            ></iframe>
+          </div>
+          <br /> <br /> <br />
+          <h2>Playing Game</h2>
+          <div class="container">
+            <iframe
+              class="responsive-iframe"
+              title="beatsaber"
+              src="https://player.vimeo.com/video/375063587"
+              width="640px"
+              height="360px"
+              frameBorder="0"
+              allow="autoplay; fullscreen"
+            ></iframe>
+          </div>
+          <br /> <br /> <br />
+          <h2>Do something.. fun!</h2>
+          <div class="container">
+            <iframe
+              class="responsive-iframe"
+              title="play kalimba"
+              src="https://player.vimeo.com/video/375051982"
+              width="640px"
+              height="360px"
+              frameBorder="0"
+              allow="autoplay; fullscreen"
+            ></iframe>
+          </div>
+          <br />
+          <br />
+          <br />
+          <br />
+        </ContentArticle>
       </ContentWrapper>
     </>
   );
@@ -64,7 +73,7 @@ const ContentImage = styled.div`
 const ContentInfo = styled.div`
   position: relative;
   top: -40px;
-  
+
   span {
     font-size: 48px;
   }
@@ -80,5 +89,22 @@ const ContentArticle = styled.div`
 
   h2 {
     color: #23255f;
+  }
+
+  .container {
+    position: relative;
+    overflow: hidden;
+    width: 100%;
+    padding-top: 56.25%; /* 16:9 Aspect Ratio (divide 9 by 16 = 0.5625) */
+  }
+
+  .responsive-iframe {
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    width: 100%;
+    height: 100%;
   }
 `;
